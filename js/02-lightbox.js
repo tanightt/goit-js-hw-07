6309d4803325e184cde171cf901d5ec898628a18
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-05-24 14:50:01 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+onCreateGalleryCards();
+
+galleryList.addEventListener('click', onGalleryEvent);
+
+function onCreateGalleryCards() {
+    let galleryCards = '';
+
+    galleryItems.forEach(item => {
+        const galleryCard = `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>`
+    galleryCards += galleryCard;
+}); 
+    galleryList.insertAdjacentHTML("afterbegin", galleryCards);
+}
+
+function onGalleryEvent(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
